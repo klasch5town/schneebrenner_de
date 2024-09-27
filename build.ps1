3ef400a18c5627f78c5858568fe2c08ae21840e8
Klaus Scharr <klaus.scharr@gmx.de>
2024-09-27 12:29:09 +0200

    start to rework build script

--- build.ps1
+++ build.ps1
@@ -5,6 +5,39 @@
 # 	[string]$SERVER="0.0.0.0",
 # 	[string]$PORT
 # )
+function Get-IniContent ($filePath)
+{
+    $ini = @{}
+    switch -regex -file $FilePath
+    {
+        “^\[(.+)\]” # Section
+        {
+            $section = $matches[1]
+            $ini[$section] = @{}
+            $CommentCount = 0
+        }
+        “^(;.*)$” # Comment
+        {
+            $value = $matches[1]
+            $CommentCount = $CommentCount + 1
+            $name = “Comment” + $CommentCount
+            $ini[$section][$name] = $value
+        }
+        “(.+?)\s*=(.*)” # Key
+        {
+            $name,$value = $matches[1..2]
+            $ini[$section][$name] = $value
+        }
+    }
+    return $ini
+}
+
+$SettingsFilePath="~/.buildbot/settings.ini"
+if (Test-Path $SettingsFilePath) {
+	$Settings=Get-IniContent $SettingsFilePath
+	foreach ($item in $Settings["SCHNEEBRENNER"]) { 
+		Write-Output $item }
+}
 $DEBUG=$false
 $RELATIVE=$false
 $PELICANOPTS=""
@@ -24,10 +57,10 @@ $OUTPUTDIR="${BASEDIR}/output"
 $CONFFILE="${BASEDIR}/pelicanconf.py"
 $PUBLISHCONF="${BASEDIR}/publishconf.py"
 
-$SSH_HOST="schneebrenner.de"
-$SSH_PORT=2244
-$SSH_USER=
-$SSH_TARGET_DIR="/home/websezrvz/html/schneebrenner_de"
+$SSH_HOST=$Settings["SCHNEEBRENNER"]["server"]
+$SSH_PORT=$Settings["SCHNEEBRENNER"]["port"]
+$SSH_USER=$Settings["SCHNEEBRENNER"]["user"]
+$SSH_TARGET_DIR=$Settings["SCHNEEBRENNER"]["target_dir"]
 
 
 if ($DEBUG) {
@@ -48,17 +81,17 @@ function Get-Help {
 	Write-Output 'Makefile for a pelican Web site                                           '
 	Write-Output '                                                                          '
 	Write-Output 'Usage:                                                                    '
-	Write-Output '   make html                           (re)generate the web site          '
-	Write-Output '   make clean                          remove the generated files         '
-	Write-Output '   make regenerate                     regenerate files upon modification '
-	Write-Output '   make publish                        generate using production settings '
-	Write-Output '   make serve [PORT=8000]              serve site at http://localhost:8000'
-	Write-Output '   make serve-global [SERVER=0.0.0.0]  serve (as root) to $(SERVER):80    '
-	Write-Output '   make devserver [PORT=8000]          serve and regenerate together      '
-	Write-Output '   make devserver-global               regenerate and serve on 0.0.0.0    '
-	Write-Output '   make ssh_upload                     upload the web site via SSH        '
-	Write-Output '   make sftp_upload                    upload the web site via SFTP       '
-	Write-Output '   make rsync_upload                   upload the web site via rsync+ssh  '
+	Write-Output '   build html                           (re)generate the web site          '
+	Write-Output '   build clean                          remove the generated files         '
+	Write-Output '   build regenerate                     regenerate files upon modification '
+	Write-Output '   build publish                        generate using production settings '
+	Write-Output '   build serve [PORT=8000]              serve site at http://localhost:8000'
+	Write-Output '   build serve-global [SERVER=0.0.0.0]  serve (as root) to $(SERVER):80    '
+	Write-Output '   build devserver [PORT=8000]          serve and regenerate together      '
+	Write-Output '   build devserver-global               regenerate and serve on 0.0.0.0    '
+	Write-Output '   build ssh_upload                     upload the web site via SSH        '
+	Write-Output '   build sftp_upload                    upload the web site via SFTP       '
+	Write-Output '   build rsync_upload                   upload the web site via rsync+ssh  '
 	Write-Output '                                                                          '
 	Write-Output 'Set the DEBUG variable to 1 to enable debugging, e.g. make DEBUG=1 html   '
 	Write-Output 'Set the RELATIVE variable to 1 to enable relative urls                    '
@@ -93,6 +126,9 @@ function Get-Serve {
 
 # ssh_upload: publish
 # 	scp -P $(SSH_PORT) -r "$(OUTPUTDIR)"/* "$(SSH_USER)@$(SSH_HOST):$(SSH_TARGET_DIR)"
+function ssh-upload {
+ 	scp -P $SSH_PORT -r "${OUTPUTDIR}"/* "${SSH_USER}@${SSH_HOST}:${SSH_TARGET_DIR}"
+}
 
 # sftp_upload: publish
 # 	printf 'put -r $(OUTPUTDIR)/*' | sftp $(SSH_USER)@$(SSH_HOST):$(SSH_TARGET_DIR)
